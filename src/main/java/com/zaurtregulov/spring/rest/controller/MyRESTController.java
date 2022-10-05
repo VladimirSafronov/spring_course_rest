c6dcdaf58a3ddc6c137538e815b35575d187c373
VladimirSafronov <safron5000@mail.ru>
2022-10-05 11:15:38 +0300

    added getEmployee(int id) in MyRESTController

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -5,6 +5,7 @@ import com.zaurtregulov.spring.rest.service.EmployeeService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,13 @@ public class MyRESTController {
   @GetMapping("/employees")
   public List<Employee> showAllEmployees() {
     return employeeService.getAllEmployees();
+  }
+
+  //Аннотация @PathVariable используется для получения значения переменной из адреса запроса
+  @GetMapping("/employees/{id}")
+  public Employee getEmployee (@PathVariable int id) {
+    //возвращается документ JSON,в кот.импортируется объект Employee благодаря
+    //Spring(jackson databind)
+    return employeeService.getEmployee(id);
   }
 }

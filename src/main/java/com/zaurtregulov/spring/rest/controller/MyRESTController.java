450c508ce555b8c9552bd97e3fc22fbe197edea2
VladimirSafronov <safron5000@mail.ru>
2022-10-09 09:03:14 +0300

    created PUT method updateEmployee()

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,8 +44,20 @@ public class MyRESTController {
   }
 
   @PostMapping("/employees")
+  //при помощи @RequestBody мы получаем работника из тела запроса
+  //(достигается при помощи spring и проекта jackson)
   public Employee addNewEmployee(@RequestBody Employee employee) {
+
     employeeService.saveEmployee(employee);
     return employee;
+
+  }
+
+  @PutMapping("/employees")
+  public Employee updateEmployee(@RequestBody Employee employee) {
+
+    employeeService.saveEmployee(employee);
+    return employee;
+
   }
 }
